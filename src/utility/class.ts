cba2b8603d6e33f1cbf5186213f98a459749f49a
i-play-lf2 <914637118@qq.com>
2023-06-25 07:48:37 +0000

    rename

--- src/utility/class.ts
+++ src/utility/class.ts
@@ -2,7 +2,7 @@ import { Newable } from "src/type/function";
 
 export function superClassOf(x: Newable): Newable | null {
   const prototype = Object.getPrototypeOf(x);
-  return endOfSuperClass === prototype ? null : prototype;
+  return endOfFunction === prototype ? null : prototype;
 }
 
-const endOfSuperClass = Object.getPrototypeOf(Function);
+const endOfFunction = Object.getPrototypeOf(Function);
